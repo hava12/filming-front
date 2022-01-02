bf1acd986f638ff5717f6a82a356c6d77eb95853
hava12 <hava12@naver.com>
2022-01-02 12:35:15 +0900

    useState, useEffect, Component 적용

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./template/Header";
+import Counter from "./components/Counter";
 
 function App() {
+    useEffect(() => {
+        console.log("effect");
+    });
+    console.log("rendering");
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    const onSubmit = () => {
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log(username, password);
         alert("submitted");
     };
     return (
         <div className="App">
             <Header />
-            <input placeholder="Username" /> <br />
-            <input placeholder="Password" /> <br />
-            <button onClick={onSubmit}> </button>
+            <form onSubmit={onSubmit}>
+                <input placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} /> <br />
+                <input placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} /> <br />
+                <button type="submit">Login</button>
+            </form>
+            <Counter />
+            <Counter />
         </div>
     );
 }
---
+++ src/components/Counter.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount(count + 1);
+    };
+    return (
+        <div>
+            <button onClick={increment}>Count {count} </button>
+        </div>
+    );
+};
+
+export default Counter;
