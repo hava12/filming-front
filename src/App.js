fc62da1b3dbfb7da8f624ce3677e7f6ddd5692ec
hava12 <hava12@naver.com>
2022-01-03 20:58:45 +0900

    React 연습

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 import Header from "./template/Header";
 import Counter from "./components/Counter";
+import Movie from "./components/Movie";
+import MovieForm from "./components/MovieForm";
 
 function App() {
     useEffect(() => {
@@ -10,12 +12,41 @@ function App() {
     console.log("rendering");
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [condition, setCondition] = useState(false);
+
+    const toggle = () => {
+        setCondition(!condition);
+    };
+
+    useEffect(() => {
+        console.log(condition);
+    }, [condition]);
 
     const onSubmit = (e) => {
         e.preventDefault();
         console.log(username, password);
         alert("submitted");
     };
+
+    const renderCondition = condition ? "True" : "False";
+
+    const [movies, setMovies] = useState([]);
+
+    const removeMovie = (id) => {
+        setMovies(movies.filter((movie) => movie.id !== id));
+    };
+
+    const renderMovies =
+        movies.length > 0
+            ? movies.map((movie) => {
+                  return <Movie movie={movie} key={movie.id} removeMovie={removeMovie} />;
+              })
+            : "추가된 영화가 없습니다.";
+
+    const addMovie = (movie) => {
+        setMovies([...movies, movie]);
+    };
+
     return (
         <div className="App">
             <Header />
@@ -25,7 +56,13 @@ function App() {
                 <button type="submit">Login</button>
             </form>
             <Counter />
-            <Counter />
+            <Counter click="click1" />
+            <Counter click="click2" />
+            <div>{renderCondition}</div>
+            {condition ? <div>True</div> : <div>False</div>}
+            <button onClick={toggle}>Toggle</button>'
+            <MovieForm addMovie={addMovie} />
+            {renderMovies}
         </div>
     );
 }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 
-const Counter = () => {
+const Counter = (props) => {
     const [count, setCount] = useState(0);
 
     const increment = () => {
         setCount(count + 1);
     };
+    const clickString = props.click || "Click";
     return (
         <div>
-            <button onClick={increment}>Count {count} </button>
+            <button onClick={increment}>
+                {clickString} {count}
+            </button>
         </div>
     );
 };
