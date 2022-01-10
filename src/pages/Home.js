fb1ca16d6b489e733a9210455408aacdc8d34776
dongyeop <hava12@naver.com>
2022-01-11 00:13:48 +0900

    UI 변경

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -19,11 +19,6 @@ const Home = () => {
 
     const join = () => {
         alert('회원가입 Modal')
-        if(joinFlag === "show") {
-            setJoinFlag("")
-        } else {
-            setJoinFlag("show")
-        }
     }
 
     return (
@@ -37,13 +32,26 @@ const Home = () => {
                 <button type="submit" className="btn btn-success m-2" >Login</button><br/>
                 <Link to={`/join`}>비밀번호를 잃어버리셨나요?</Link><br/>
                 <hr/>
-                <button onClick={join} className="btn btn-secondary m-2" >Join</button>
             </form>
+            <button type="button" className="btn btn-secondary m-2" data-bs-toggle="modal" data-bs-target="#joinModal">Join</button>
 
-            <div className={"modal-dialog modal-dialog-centered"} >
-            ...
+            <div className="modal fade" id="joinModal" tabIndex="-1" aria-labelledby="exampleModalCenterTitle" style={{display: "none"}} aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalCenterTitle">Modal title</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <p>This is a vertically centered modal.</p>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
             </div>
-
         </>
     ) 
 };
