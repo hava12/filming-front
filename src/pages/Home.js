69e977c5fe871205621e3583bece1be63f136126
dongyeop <hava12@naver.com>
2022-01-10 22:31:15 +0900

    Home page ui

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,30 @@
 import React, {useState} from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Home = () => {
 
-    const [username, setUsername] = useState("");
+    const [userId, setUserId] = useState("");
     const [password, setPassword] = useState("");
+    const [joinFlag, setJoinFlag] = useState("show");
 
     const login = (e) => {
         e.preventDefault();
-        const userInfo = { username: username, password: password };
+        const userInfo = { userId: userId, password: password };
         axios.post("http://localhost:8000/user-service/v1/login", userInfo).then((res) => {
             console.log(res);
         });
-        console.log(username, password);
+        console.log(userId, password);
     };
+
+    const join = () => {
+        alert('회원가입 Modal')
+        if(joinFlag === "show") {
+            setJoinFlag("")
+        } else {
+            setJoinFlag("show")
+        }
+    }
 
     return (
         <>
@@ -21,10 +32,18 @@ const Home = () => {
             <br/><br/><br/>
             <h1>Login</h1>
             <form onSubmit={login}>
-                <input placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} /> <br />
-                <input placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} /> <br />
-                <button type="submit">Login</button>
+                <input placeholder="UserId" value={userId} onChange={(e) => setUserId(e.target.value)} /> <br />
+                <input placeholder="Password" className="mt-2" value={password} onChange={(e) => setPassword(e.target.value)} /> <br />
+                <button type="submit" className="btn btn-success m-2" >Login</button><br/>
+                <Link to={`/join`}>비밀번호를 잃어버리셨나요?</Link><br/>
+                <hr/>
+                <button onClick={join} className="btn btn-secondary m-2" >Join</button>
             </form>
+
+            <div className={"modal-dialog modal-dialog-centered"} >
+            ...
+            </div>
+
         </>
     ) 
 };
