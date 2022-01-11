18a5924878dbe769c3b07efe97b0a8b0070bb814
dongyeop <hava12@naver.com>
2022-01-12 00:03:35 +0900

    Home UI

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,8 @@ const Home = () => {
 
     const [userId, setUserId] = useState("");
     const [password, setPassword] = useState("");
-    const [joinFlag, setJoinFlag] = useState("show");
+    const [joinUserId, setJoinUserId] = useState("");
+    const [joinPassword, setJoinPassword] = useState("");
 
     const login = (e) => {
         e.preventDefault();
@@ -29,25 +30,26 @@ const Home = () => {
             <form onSubmit={login}>
                 <input placeholder="UserId" value={userId} onChange={(e) => setUserId(e.target.value)} /> <br />
                 <input placeholder="Password" className="mt-2" value={password} onChange={(e) => setPassword(e.target.value)} /> <br />
-                <button type="submit" className="btn btn-success m-2" >Login</button><br/>
-                <Link to={`/join`}>비밀번호를 잃어버리셨나요?</Link><br/>
+                <button type="submit" className="btn btn-success m-2" >로그인</button><br/>
+                <Link to={`/findId`}>비밀번호를 잃어버리셨나요?</Link><br/>
                 <hr/>
             </form>
-            <button type="button" className="btn btn-secondary m-2" data-bs-toggle="modal" data-bs-target="#joinModal">Join</button>
+            <button type="button" className="btn btn-secondary m-2" data-bs-toggle="modal" data-bs-target="#joinModal">회원가입</button>
 
             <div className="modal fade" id="joinModal" tabIndex="-1" aria-labelledby="exampleModalCenterTitle" style={{display: "none"}} aria-hidden="true">
                 <div className="modal-dialog modal-dialog-centered">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalCenterTitle">Modal title</h5>
+                            <h5 className="modal-title" id="exampleModalCenterTitle">회원가입</h5>
                             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div className="modal-body">
-                            <p>This is a vertically centered modal.</p>
+                            <input className="m-2" placeholder="UserId" value={joinUserId} onChange={(e) => setJoinUserId(e.target.value)} /> <br />
+                            <input className="mb-2" placeholder="Password" value={joinPassword} onChange={(e) => setJoinPassword(e.target.value)} /> <br />
                         </div>
                         <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" className="btn btn-primary">Save changes</button>
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">취소</button>
+                            <button type="button" className="btn btn-primary">가입</button>
                         </div>
                     </div>
                 </div>
